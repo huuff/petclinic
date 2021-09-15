4d74a45391b6d235d83316de16c4a08f4c6fb38a
Haf <haf@protonmail.ch>
2021-09-16 00:53:08 +0200

    preventing binding of owner id

--- src/main/java/xyz/haff/petclinic/controller/OwnerController.java
+++ src/main/java/xyz/haff/petclinic/controller/OwnerController.java
@@ -3,6 +3,7 @@ package xyz.haff.petclinic.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import xyz.haff.petclinic.models.Owner;
 import xyz.haff.petclinic.models.Pet;
@@ -13,14 +14,20 @@ import xyz.haff.petclinic.repositories.OwnerRepository;
 @RequestMapping("/owners")
 public class OwnerController {
     private static final String OWNER_CREATE_OR_UPDATE_FORM = "owners/edit";
+    private static final String OWNER_LIST = "owners/list";
 
     private final OwnerRepository repository;
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
 
     @GetMapping("/list")
     public String list(Model model) {
         model.addAttribute("owners", repository.findAll());
 
-        return "owners/list";
+        return OWNER_LIST;
     }
 
     @GetMapping("/{id}/edit")
@@ -30,6 +37,14 @@ public class OwnerController {
         return OWNER_CREATE_OR_UPDATE_FORM;
     }
 
+    @PostMapping("/{id}/edit")
+    public String saveOrUpdate(@PathVariable String id, @ModelAttribute Owner owner) {
+        owner.setId(id);
+        repository.save(owner);
+
+        return "redirect:/" + OWNER_LIST;
+    }
+
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("owner", new Owner());
@@ -37,12 +52,14 @@ public class OwnerController {
         return OWNER_CREATE_OR_UPDATE_FORM;
     }
 
-    @PostMapping("/edit")
-    public String saveOrUpdate(@ModelAttribute Owner owner) {
-        var savedOwner = repository.save(owner);
+    @PostMapping("/create")
+    public String create(@ModelAttribute Owner owner) {
+        repository.save(owner);
 
-        return "redirect:/owners/list";
+        return "redirect:/" + OWNER_LIST;
     }
+
+
 
     @GetMapping("/{id}/add_pet")
     public String list(@PathVariable String id, Model model) {
