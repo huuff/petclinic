37185057ec193b7b786d431ef7b15388fe1caeb6
Haf <haf@protonmail.ch>
2021-09-29 09:21:16 +0200

    using kotlin for OwnerRepository

---
+++ petclinic-data/src/main/kotlin/xyz/haff/petclinic/repositories/OwnerRepository.kt
@@ -0,0 +1,24 @@
+package xyz.haff.petclinic.repositories
+
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.r2dbc.repository.R2dbcRepository
+import reactor.core.publisher.Flux
+import xyz.haff.petclinic.models.Owner
+import java.util.*
+
+interface OwnerRepository : R2dbcRepository<Owner, UUID>, OwnerRepositoryCustom<Owner> {
+
+    @Query("""
+        SELECT 
+            o.id as OWNER_ID, 
+            o.version as OWNER_VERSION,
+            o.first_name as FIRST_NAME,
+            o.last_name as LAST_NAME,
+            u.id as USER_ID,
+            u.version as USER_VERSION, 
+            u.username as USERNAME,
+            u.password as PASSWORD
+            FROM owner as o JOIN user as u ON o.user=u.id
+    """)
+    override fun findAll(): Flux<Owner>
+}
--- petclinic-web/src/main/java/xyz/haff/petclinic/PetclinicApplication.java
+++ petclinic-web/src/main/java/xyz/haff/petclinic/PetclinicApplication.java
@@ -2,8 +2,6 @@ package xyz.haff.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 public class PetclinicApplication {
