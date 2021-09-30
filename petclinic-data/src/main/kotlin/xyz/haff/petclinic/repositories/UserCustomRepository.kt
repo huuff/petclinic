f5edf57733e497537928d35f568831f3f584827f
Haf <haf@protonmail.ch>
2021-09-30 14:42:21 +0200

    I don't think I can merge my custom repositories

--- petclinic-data/src/main/kotlin/xyz/haff/petclinic/repositories/UserCustomRepository.kt
+++ petclinic-data/src/main/kotlin/xyz/haff/petclinic/repositories/UserCustomRepository.kt
@@ -2,7 +2,6 @@ package xyz.haff.petclinic.repositories
 
 import reactor.core.publisher.Mono
 
-// TODO: All my custom repositories are exactly the same but I don't seem to manage to fix it otherwise
 interface UserCustomRepository<T> {
 
     fun <S : T> save(user: S): Mono<S>
--- petclinic-data/src/main/kotlin/xyz/haff/petclinic/repositories/VetCustomRepository.kt
+++ petclinic-data/src/main/kotlin/xyz/haff/petclinic/repositories/VetCustomRepository.kt
@@ -2,7 +2,6 @@ package xyz.haff.petclinic.repositories
 
 import reactor.core.publisher.Mono
 
-// TODO: merge this with `OwnerCustomRepository` since they are exactly the same thing
 interface VetCustomRepository<T> {
 
     fun <S : T> save(vet : S): Mono<S>
