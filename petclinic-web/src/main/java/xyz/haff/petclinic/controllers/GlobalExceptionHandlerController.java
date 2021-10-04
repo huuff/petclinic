a849a0e79920c9fc3b2f66d18bc05e5078828b97
Haf <haf@protonmail.ch>
2021-10-05 00:25:10 +0200

    I guess I'm logging the exception but haven't tested it

--- petclinic-web/src/main/java/xyz/haff/petclinic/controllers/GlobalExceptionHandlerController.java
+++ petclinic-web/src/main/java/xyz/haff/petclinic/controllers/GlobalExceptionHandlerController.java
@@ -1,15 +1,17 @@
 package xyz.haff.petclinic.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import xyz.haff.petclinic.exceptions.NotFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandlerController {
 
-    // TODO: Log exceptions?
     @ExceptionHandler(NotFoundException.class)
-    public String notFound() {
+    public String notFound(Exception exception) {
+        log.error(exception.getMessage());
         return "errors/404";
     }
 }
