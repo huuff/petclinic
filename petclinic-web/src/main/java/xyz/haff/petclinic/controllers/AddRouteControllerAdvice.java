63babfa75acb29b14aa4b473f0154f4cbb2a6a6c
Haf <haf@protonmail.ch>
2021-10-15 15:37:01 +0200

    marking active route in navbar

---
+++ petclinic-web/src/main/java/xyz/haff/petclinic/controllers/AddRouteControllerAdvice.java
@@ -0,0 +1,34 @@
+package xyz.haff.petclinic.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ * Controller advice to get the route and add it to the model for all controllers. This is used by the navbar to
+ * show which route is active. Feels a little hacky but eh
+ */
+
+@ControllerAdvice
+public class AddRouteControllerAdvice {
+
+    @ModelAttribute
+    public void addRouteToModel(Model model, WebRequest webRequest) {
+        var uri = ((ServletWebRequest) webRequest).getRequest().getRequestURI();
+
+        model.addAttribute("route", getRoute(uri));
+    }
+
+    private Route getRoute(String uri) {
+        if (uri.contains("owners"))
+            return Route.OWNERS;
+        else if (uri.contains("profile"))
+            return Route.PROFILE;
+        else if (uri.contains("vets"))
+            return Route.VETS;
+        else
+            return Route.DEFAULT;
+    }
+}
---
+++ petclinic-web/src/main/java/xyz/haff/petclinic/controllers/Route.java
@@ -0,0 +1,5 @@
+package xyz.haff.petclinic.controllers;
+
+public enum Route {
+    OWNERS, VETS, PROFILE, DEFAULT;
+}
