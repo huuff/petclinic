3f134a8c58debddfcd35cb873a60e4c4246ba958
Haf <haf@protonmail.ch>
2021-10-22 10:27:28 +0200

    showing appointed visits for both owners and vets in index

--- petclinic-data/src/main/java/xyz/haff/petclinic/repositories/VisitRepository.java
+++ petclinic-data/src/main/java/xyz/haff/petclinic/repositories/VisitRepository.java
@@ -3,7 +3,10 @@ package xyz.haff.petclinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 import xyz.haff.petclinic.models.Visit;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface VisitRepository extends CrudRepository<Visit, UUID> {
+
+    List<Visit> findByPet_OwnerId(UUID ownerId);
 }
--- petclinic-web/src/main/java/xyz/haff/petclinic/services/VisitService.java
+++ petclinic-web/src/main/java/xyz/haff/petclinic/services/VisitService.java
@@ -7,16 +7,23 @@ import xyz.haff.petclinic.repositories.VisitRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class VisitService {
+    private final VisitRepository visitRepository;
+
     public List<Visit> filterPast(List<Visit> visits) {
         return visits.stream().filter(visit -> visit.getDateTime().isBefore(LocalDateTime.now())).collect(Collectors.toList());
     }
 
     public List<Visit> filterUpcoming(List<Visit> visits) {
         return visits.stream().filter(visit -> visit.getDateTime().isAfter(LocalDateTime.now())).collect(Collectors.toList());
+    }
+
+    public List<Visit> findAllOfOwner(UUID ownerId) {
+        return visitRepository.findByPet_OwnerId(ownerId);
     }
 }
